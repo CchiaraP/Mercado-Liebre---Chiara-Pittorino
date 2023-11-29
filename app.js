a9059fd8b8dd2671becd0cebd6678aa7cb463106
CchiaraP <pittorino.chiara@gmail.com>
2023-11-28 22:55:46 -0300

    app.post de login y register en app.js

--- app.js
+++ app.js
@@ -19,6 +19,14 @@ app.get('/register', (req, res) => (
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 ));
 
+app.post('/register', (req, res) => (
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+));
+
 app.get('/login', (req, res) => (
     res.sendFile(path.resolve(__dirname, './views/login.html'))
+));
+
+app.post('/login', (req, res) => (
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 ));
